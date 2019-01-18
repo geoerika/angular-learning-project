91be01d63f02e2b617e7d21268f4f1af1e4e66db
Erika Szabo <erika.szabo.geo@gmail.com>
2019-01-18 19:53:58 +0000

    created rental-list and rental-list-item compoenents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { RentalComponent } from './rental/rental.component';
 import { TempComponent } from './temp/temp.component';
+import { RentalListComponent } from './rental/rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component';
 
 const routes: Routes = [
 {path:'', component: RentalComponent},
@@ -18,7 +20,9 @@ const routes: Routes = [
     AppComponent,
     HeaderComponent,
     RentalComponent,
-    TempComponent
+    TempComponent,
+    RentalListComponent,
+    RentalListItemComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
