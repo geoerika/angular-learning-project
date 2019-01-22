691f51c43449ebc6141990e9f99745dbbe21a689
Erika Szabo <erika.szabo.geo@gmail.com>
2019-01-22 23:38:18 +0000

    displayed details of an individual rental when clicking on the rental link

--- src/app/rental/rental-detail/rental-detail.component.ts
+++ src/app/rental/rental-detail/rental-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { RentalService } from '../shared/rental.service';
+import { Rental } from '../shared/rental.model';
 
 @Component({
   selector: 'bwm-rental-detail',
@@ -8,17 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RentalDetailComponent implements OnInit {
 
-  currentId: string;
+  rental: Rental;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private rentalService: RentalService) {
   }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params) => {
-        console.log('params: ', params);
-        this.currentId = params['rentalId']; //rentalId comes from rental module path; params is a list of parameters
+        this.getRental(params['rentalId']);
       })
   }
 
+  getRental(rentalId: string) {
+      this.rentalService.getRentalById(rentalId).subscribe(
+        (rental: Rental) => {
+           this.rental = rental;
+      });
+  }
 }
