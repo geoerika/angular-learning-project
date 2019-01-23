f876a3256d865c3f37a672551e16cf6491c093ae
Erika Szabo <erika.szabo.geo@gmail.com>
2019-01-23 16:08:16 +0000

    created the first endpoint on server side

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-index.js
+const express = require('express');
+
+const app = express();
+
+app.get('/rentals', function(req, res) { //handler function
+  res.json({'success': true});
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, function() {
+  console.log('Server running...');
+});
