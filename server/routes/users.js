2e020849aae9774c105a1bb7f8f3048840cac164
Erika Szabo <erika.szabo.geo@gmail.com>
2019-01-30 20:20:01 +0000

    created route file for users

---
+++ server/routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const Rental = require('../models/rental');
+const User = require('../models/user');
+
+
+router.post('/auth', function(req, res) { //handler function {
+
+});
+
+router.post('/register', function(req,res) {
+
+});
+
+module.exports = router;
