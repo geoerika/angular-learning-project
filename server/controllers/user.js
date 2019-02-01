59da6cbbe8a92a01be435dd11f24d843c696e6f9
Erika Szabo <erika.szabo.geo@gmail.com>
2019-02-01 22:29:51 +0000

    implemented checking if user exists at registration

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.auth =function(req, res) { //handler function for routes
 
 }
@@ -12,7 +14,30 @@ exports.register = function(req, res) {
 
   if (password !== passwordConfirmation) {
     return res.status(422).send({errors: [{title: 'Invalid passsword!', detail: 'Password is not the same as confirmation!'}]});
-}
+  }
 
-  res.json({username, email});
+  User.findOne({email}, function(err, existingUser) {
+    if (err) {
+      res.status(422).send({'mongose': 'handle mongoose errors in next lecture'});
+    }
+
+    if (existingUser) {
+      res.status(422).send({errors: [{title: 'Invalid email!', detail: 'User with this email already exists!'}]});
+    }
+
+    const user = new User({
+      username,
+      email,
+      password
+    });
+
+    user.save(function(err) {
+      if (err) {
+      res.status(422).send({'mongose': 'handle mongoose errors in next lecture'});
+      }
+
+      res.json({'registered': true});
+    });
+  }); //similar to {email: email}
+
 }
