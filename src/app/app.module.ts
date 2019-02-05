4c0783effa53a8f5caaa678af043bde146c0ce90
Erika Szabo <erika.szabo.geo@gmail.com>
2019-02-05 17:56:52 +0000

    deleted unecessery lines

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { RentalComponent } from './rental/rental.component';
+
 import { RentalModule } from './rental/rental.module';
+import { AuthModule } from './auth/auth.module';
 
 const routes: Routes = [
 {path:'', redirectTo: '/rentals', pathMatch: 'full'}
@@ -21,8 +21,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule,
-    RentalModule
+    RentalModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
