a49c8baef42d9fc3a6fa4e06f4c4802cfbba19a9
Erika Szabo <erika.szabo.geo@gmail.com>
2019-02-05 19:03:45 +0000

    created formData variable to link it to the form data in the html page

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  formData: any = {};
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  register() {
+    console.log(this.formData);
+  }
+
 }
+
+//[(ngModel)]="formData.username" in htm file is two ways data binding
+//[ngModel]="formData.username" is one way data binding, it will get the value from ngOnInit
