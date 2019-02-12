8483736d2cbec4eec3b8166e0292901f13b5ac88
Erika Szabo <erika.szabo.geo@gmail.com>
2019-02-12 23:44:49 +0000

    implemented login and saveToken constuctor methods

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import 'rxjs/Rx'; //to have access to map function
 
 
 @Injectable()
@@ -10,5 +11,18 @@ export class AuthService {
 
   public register(userData: any): Observable<any> {
     return this.http.post('/api/v1/users/register', userData);
+  }
+
+  public login(userData: any): Observable<any> {
+    return this.http.post('/api/v1/users/auth', userData).map(
+      (token) => {
+        debugger;
+        return this.saveToken(token);
+      });
+  }
+
+  private saveToken(token: any): any {
+    localStorage.setItem('bwm_auth', token);
+    return token;
   }
 }
