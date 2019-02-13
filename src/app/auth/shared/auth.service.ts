6dc878198c51aed0bc9beb7e54c3b308d1bf48b6
Erika Szabo <erika.szabo.geo@gmail.com>
2019-02-13 20:00:02 +0000

    checked the token to see if it expired or not

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import * as moment from 'moment';
 import 'rxjs/Rx'; //to have access to map function
 
 const jwt = new JwtHelperService();
@@ -17,7 +18,20 @@ export class AuthService {
 
   private decodedToken;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.decodedToken = JSON.parse(localStorage.getItem('bwm_meta')) || new DecodedToken();
+  }
+
+  private saveToken(token: any): any {
+    this.decodedToken = jwt.decodeToken(token);
+    localStorage.setItem('bwm_auth', token);
+    localStorage.setItem('bwm_meta', JSON.stringify(this.decodedToken));
+    return token;
+  }
+
+  private getExpiration() {
+    return moment.unix(this.decodedToken.exp); //used moment.unix because decodedToken.exp is in msec
+  }
 
   public register(userData: any): Observable<any> {
     return this.http.post('/api/v1/users/register', userData);
@@ -28,11 +42,7 @@ export class AuthService {
       (token) => this.saveToken(token));
   }
 
-  private saveToken(token: any): any {
-    this.decodedToken = jwt.decodeToken(token);
-    localStorage.setItem('bwm_auth', token);
-    localStorage.setItem('bwm_meta', JSON.stringify(this.decodedToken));
-
-    return token;
+  public isAuthenticated(): boolean {
+    return moment().isBefore(this.getExpiration());
   }
 }
