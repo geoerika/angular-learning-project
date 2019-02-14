8a510663fecbd9150a86422f8fab8e2d40c23aa3
Erika Szabo <erika.szabo.geo@gmail.com>
2019-02-14 21:30:35 +0000

    created auth.guard.ts to prevent unauthoriezed access on rental page

---
+++ src/app/auth/shared/auth.guard.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { CanActivate,
+         ActivatedRouteSnapshot,
+         RouterStateSnapshot } from '@angular/router';
+
+import { AuthService }      from './auth.service';
+
+@Injectable()
+
+export class AuthGuard implements CanActivate {
+  private url: string;
+  constructor(private auth: AuthService,
+              private router: Router) {}
+
+  private handleAuthState(): boolean {
+
+    if (this.isLoginOrRegister()) {
+      this.router.navigate(['/rentals']);
+      return false;
+    }
+    return true;
+  }
+
+  private handleNotAuthState(): boolean {
+    if (this.isLoginOrRegister()) {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
+
+  private isLoginOrRegister(): boolean {
+    if (this.url.includes('login') || this.url.includes('register')) {
+      return true;
+    }
+    return false;
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    this.url = state.url; //state.url is the url we are navigating to
+
+    if (this.auth.isAuthenticated()) {
+      return this.handleAuthState();
+    }
+    return this.handleNotsAuthState();
+  }
+}
