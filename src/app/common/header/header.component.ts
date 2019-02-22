1244c150f8eeb61a0047ed700f8db3b5bc987798
Erika Szabo <erika.szabo.geo@gmail.com>
2019-02-22 21:13:02 +0000

    made auth public

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 
 export class HeaderComponent {
 
-  constructor(private auth: AuthService,
+  constructor(public auth: AuthService,
               private router: Router) {}
 
   logout() {
