60ae15619fea5c4fb19eb48d7f5aebec576c3fd6
Erika Szabo <erika.szabo.geo@gmail.com>
2019-02-28 19:25:38 +0000

    commented out the seeding of database

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const rentalRoutes = require('./routes/rentals'),
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
   const fakeDb = new FakeDb();
-  fakeDb.seedDb();
+  // fakeDb.seedDb();
 });
 
 
