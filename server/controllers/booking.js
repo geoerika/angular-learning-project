be7e3318eb9a8975ac969000f3883d5a899712fe
Erika Szabo <erika.szabo.geo@gmail.com>
2019-02-28 19:24:58 +0000

    implemented createBooking function

--- server/controllers/booking.js
+++ server/controllers/booking.js
@@ -1,5 +1,28 @@
-exports.createBooking =function(req, res) {
+const Booking = require('../models/booking');
+const Rental = require('../models/rental');
+const { normalizedErrors } = require('../helpers/moongose');
 
-  res.json({'createBooking': 'ok'});
+exports.createBooking = function(req, res) {
+  const { startAt, endAt, totalPrice, guests, days, rental } = req.body;
+  const user = res.locals.user;
 
+  const booking = new Booking({ startAt, endAt, totalPrice, guests, days });
+
+  Rental.findById(rental._id) //if we just call this we would get the rental but wothout the bookings or user
+        .populate('bookings') //therefore we have to use populate to get this info
+        .populate('user')
+        .exec(function(err, foundRental) {
+          if (err) {
+            res.status(422).send({errors: normalizedErrors(err.errors)});
+          }
+
+          if (foundRental.user.id === user.id) {
+            return res.status(422).send({errors: [{title: 'Invalid User!', detail: 'Cannot create Booking on your Rental!'}]});
+          }
+
+          //Check here for valid booking
+
+          return res.json({booking, foundRental});
+
+        })
 }
