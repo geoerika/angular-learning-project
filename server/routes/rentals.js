b04e0e2495bacb9efdb87eadd159a5246548ef82
Erika Szabo <erika.szabo.geo@gmail.com>
2019-03-01 03:24:24 +0000

    completed the routers

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -11,19 +11,25 @@ router.get('/secret', UserCtrl.authMiddleware, function(req, res) {
 
 
 router.get('', function(req, res) { //handler function {
-  Rental.find({}, function(err, foundRentals){
-    res.json(foundRentals);
-  });
+  Rental.find({})
+        .select('-bookings')
+        .exec(function(err, foundRentals){
+          res.json(foundRentals);
+        });
 });
 
 router.get('/:id', function(req,res) {
   const rentalId = req.params.id;
-  Rental.findById(rentalId, function(err, foundRental) {
-    if (err) {
-      res.status(422).send({errors: [{title: 'Rental Error!', details: 'Could not Find Rental!'}]})
-    }
-    res.json(foundRental);
-  });
+
+  Rental.findById(rentalId)
+        .populate('user', 'username -_id') //'-' means don't send id, restriction what to send and what not
+        .populate('bookings', 'startAt endAt -_id')
+        .exec(function(err, foundRental) {
+          if (err) {
+            return res.status(422).send({errors: [{title: 'Rental Error!', details: 'Could not Find Rental!'}]})
+          }
+          return res.json(foundRental);
+        });
 });
 
 module.exports = router;
